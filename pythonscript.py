a30bed6e3c54e57530ee086e54d55970627f3303
idoasa <95133746+idoasa@users.noreply.github.com>
2022-01-16 21:38:57 +0200

    Create pythonscript.py

---
+++ pythonscript.py
@@ -0,0 +1,7 @@
+import time
+
+SLEEP = 2
+
+print("Hello, This is a python script for docker project")
+print("Going to sleep for 2 seconds..if you want to kill the container , set SLEEP variable to 20")
+time.sleep(SLEEP)
