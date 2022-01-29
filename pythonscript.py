d2a7eb87e97ff123dddb549d8c25ab926cd456bd
idoasa <95133746+idoasa@users.noreply.github.com>
2022-01-29 16:00:16 +0200

    Update pythonscript.py

--- pythonscript.py
+++ pythonscript.py
@@ -1,6 +1,6 @@
 import time
 
-SLEEP = 2
+SLEEP = 20
 print("Hello, This is a python script for docker project")
 print(f"Going to sleep for {SLEEP} seconds..if you want to kill the python process , set SLEEP variable to 20")
 print("Waiting...")
